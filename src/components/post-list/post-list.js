723b126761f562d19b6d90e842499d5186080f1d
Daniil <thedas667@gmail.com>
2021-04-01 21:09:40 +0300

    First app is done!

--- src/components/post-list/post-list.js
+++ src/components/post-list/post-list.js
@@ -1,26 +1,27 @@
 import React from 'react';
-import {ListGroup} from 'reactstrap';
-import './post-list.css';
-import PostListItem from '../post-list-item';
 
-const PostList = ({posts, onDelete}) => {
+import PostListItem from '../post-list-item';
+import './post-list.css';
+
+const PostList = ({posts, onDelete, onToggleImportant, onToggleLiked}) => {
 
     const elements = posts.map((item) => {
-        const{id, ...itemPorps} = item
+        const {id, ...itemProps} = item;
         return (
-            <li key={id} className="list-group-item">
-                <PostListItem
-                    {...itemPorps}
-                    onDelete={() => onDelete(id)}/>
+            <li key={id} className='list-group-item'>
+                <PostListItem 
+                    {...itemProps}
+                    onDelete={() => onDelete(id)}
+                    onToggleImportant={() => onToggleImportant(id)}
+                    onToggleLiked={() => onToggleLiked(id)}/>
             </li>
         )
     });
-    
+
     return (
-        <ListGroup className="app-list">
+        <ul className="app-list list-group">
             {elements}
-        </ListGroup>
-            
+        </ul>
     )
 }
 
